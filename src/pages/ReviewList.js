7920d3f201860a76910318ff7eed4e87740e4d6e
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-03-22 19:57:11 +0900

    reviewList axios설정

--- src/pages/ReviewList.js
+++ src/pages/ReviewList.js
@@ -1,14 +1,17 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
 import {Routes,Route,Link} from "react-router-dom";
 import Review from "./Review";
 import WriteReview from "./WriteReview";
+import axios from 'axios';
 
 const Reviews = ({reviewData}) =>{
     return(
         <tr>
         <td><Link to = "/review"> {reviewData.reviewId} </Link> </td>
         <td>{reviewData.writerName}</td>
+        <td>{reviewData.reviewTitle}</td>
         <td>{reviewData.createdAt}</td>
+        <td>{reviewData.reviewLikeCount}</td>
         <Routes>
             <Route path="/review" element={<Review />} />
         </Routes>
@@ -16,21 +19,26 @@ const Reviews = ({reviewData}) =>{
     )
 }
 
-const ReviewList = () =>{
-   
-    const reviews = [
-        {reviewId : 1,
-        writerName : 'kim',
-        createdAt : '2023.01.01' },
-        {reviewId : 2,
-        writerName : 'lee',
-        createdAt : '2023.01.03'},
-        {reviewId : 3,
-        writerName : 'park',
-        createdAt : '2023.01.04'},
-    ];
 
-    return(
+
+
+const ReviewList = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        const fetchReviews = async () => {
+            try {
+                const response = await axios.get(URL_VARIABLE + "review/1");
+                setReviews(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchReviews();
+    }, []);
+
+    return (
         <table>
             <thead>
                 <tr>
@@ -39,19 +47,18 @@ const ReviewList = () =>{
                     <th>작성일자</th>
                 </tr>
             </thead>
-        
-        <tbody>
-            {reviews.map(review => <Reviews key={review.reviewId} reviewData={review} />)}
-        </tbody>
-        <div>
-        <Link to = "/writeReview"><button >리뷰작성</button></Link >
-        </div>
-        <Routes>
-            <Route path="/writeReview" element={<WriteReview />} />
-        </Routes>
-        </table>
-        
-    );
 
+            <tbody>
+                {reviews.map(review => <Reviews key={review.reviewId} reviewData={review} />)}
+            </tbody>
+            <div>
+                <Link to="/writeReview"><button >리뷰작성</button></Link>
+            </div>
+            <Routes>
+                <Route path="/writeReview" element={<WriteReview />} />
+            </Routes>
+        </table>
+    );
 }
+
 export default ReviewList;
