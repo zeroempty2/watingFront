ab8f06dc7eb7b42131ac50ffded2f874ec48e632
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-04-01 21:23:24 +0900

    메인페이지 템플릿 적용

--- src/App.js
+++ src/App.js
@@ -15,15 +15,14 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 function App() {
   return(
   <div className="App">
-
-        <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">MainPage</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-          <Nav.Link style={{ textDecoration: 'none', fontFamily: 'Arial, sans-serif' }}>
-            <Link to="/Home" style={{ color: 'black' }}>Home</Link>
+          <Nav.Link >
+            <Link to="/" style={{ textDecoration: 'none', fontFamily: 'Arial, sans-serif' , color: 'white' }}>Home</Link>
           </Nav.Link>
           <Nav.Link to = "/reviewList"> ReviewList </Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
@@ -49,7 +48,76 @@ function App() {
     <Route path="/writeReview" element={<WriteReview />} />
     <Route path="/main" element={<Main />} />
   </Routes>
+  <div>
+  <div>
+        <main className="deals">
+    <article className="sale-item">
+      <h1>Computer Starter Kit</h1>
+      <p>
+        This is the best computer money can buy, if you don’t have much money.
+      </p>
+      <ul>
+        <li>Computer</li>
+        <li>Monitor</li>
+        <li>Keyboard</li>
+        <li>Mouse</li>
+      </ul>
+      <img
+        src="https://www.w3.org/TR/css-flexbox-1/images/computer.jpg"
+        alt="You get: a white computer with matching peripherals"
+      />
+      <button>BUY NOW</button>
+    </article>
+    <article className="sale-item">
+      <h1>Printer</h1>
+      <p>Only capable of printing ASCII art.</p>
+      <ul>
+        <li>Paper and ink not included.</li>
+      </ul>
+      <img
+        src="https://www.w3.org/TR/css-flexbox-1/images/printer.png"
+        alt="You get: a dot-matrix printer."
+      />
+      <button>BUY NOW</button>
+    </article>
+    <article className="sale-item">
+      <h1>Printer 2</h1>
+      <p>A la 1989.</p>
+      <img
+        src="https://www.w3.org/TR/css-flexbox-1/images/printer.png"
+        alt="You get: a dot-matrix printer."
+      />
+      <button>BUY NOW</button>
+    </article>
+    <article className="sale-item">
+      <h1>Mouse</h1>
+      <p>
+        A computer mouse is a pointing device (hand control) that detects
+        two-dimensional motion relative to a surface. This motion is typically
+        translated into the motion of a pointer on a display, which allows a
+        smooth control of the graphical user interface. Clickity Click!
+      </p>
+      <button>BUY NOW</button>
+    </article>
+    <article className="sale-item">
+      <h1>Macintosh 8500</h1>
+      <p>
+        This is the best computer money can buy, if you don’t have much money.
+        This computer was state of the art in 1995.
+      </p>
+      <img
+        src="https://www.w3.org/TR/css-flexbox-1/images/computer.jpg"
+        alt="You get: a white computer with matching peripherals."
+      />
+      <button>BUY NOW</button>
+      
+    </article>
+  </main>
+    </div>   
   </div>
+  </div>
+
+  
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,76 @@
 //함수형 컴포넌트
 import React from "react"
+import './css/HomeStyle.css'
 
 function Home(){
-    return <h1>Home 화면 입니다</h1>;
+
+
+    <main className="deals">
+    <article className="sale-item">
+      <h1>Computer Starter Kit</h1>
+      <p>
+        This is the best computer money can buy, if you don’t have much money.
+      </p>
+      <ul>
+        <li>Computer</li>
+        <li>Monitor</li>
+        <li>Keyboard</li>
+        <li>Mouse</li>
+      </ul>
+      <img
+        src="https://www.w3.org/TR/css-flexbox-1/images/computer.jpg"
+        alt="You get: a white computer with matching peripherals"
+      />
+      <button>BUY NOW</button>
+    </article>
+    <article className="sale-item">
+      <h1>Printer</h1>
+      <p>Only capable of printing ASCII art.</p>
+      <ul>
+        <li>Paper and ink not included.</li>
+      </ul>
+      <img
+        src="https://www.w3.org/TR/css-flexbox-1/images/printer.png"
+        alt="You get: a dot-matrix printer."
+      />
+      <button>BUY NOW</button>
+    </article>
+    <article className="sale-item">
+      <h1>Printer 2</h1>
+      <p>A la 1989.</p>
+      <img
+        src="https://www.w3.org/TR/css-flexbox-1/images/printer.png"
+        alt="You get: a dot-matrix printer."
+      />
+      <button>BUY NOW</button>
+    </article>
+    <article className="sale-item">
+      <h1>Mouse</h1>
+      <p>
+        A computer mouse is a pointing device (hand control) that detects
+        two-dimensional motion relative to a surface. This motion is typically
+        translated into the motion of a pointer on a display, which allows a
+        smooth control of the graphical user interface. Clickity Click!
+      </p>
+      <button>BUY NOW</button>
+    </article>
+    <article className="sale-item">
+      <h1>Macintosh 8500</h1>
+      <p>
+        This is the best computer money can buy, if you don’t have much money.
+        This computer was state of the art in 1995.
+      </p>
+      <img
+        src="https://www.w3.org/TR/css-flexbox-1/images/computer.jpg"
+        alt="You get: a white computer with matching peripherals."
+      />
+      <button>BUY NOW</button>
+      
+    </article>
+  </main>
+
+    
+  ;
 }
 
 export default Home; 
